3d6ee440b508ca88bc336316c0920e73f8a23c91
Strohl Nicolas <strohl@c-koya.tech>
2019-02-04 16:01:17 +0100

    Ajout composant GooglePlacesSearchBar v1
    Le composant render un textinput qui fait un appel à l'api de google places à chaque saisie de texte afin de proposer à l'utilisateur des lieux correspondant à celui qu'il recherche

---
+++ GeoRally/Components/GooglePlacesSearchBar.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { TextInput, View, FlatList, Text, TouchableOpacity } from 'react-native';
+import FetchRequest from "../Tools/FetchRequest"
+
+class GooglePlacesSearchBar extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            predictions: [],
+            focusOnBar: false
+        }
+        this.requests = []
+    }
+
+    _onSearch = (item) => 
+    {
+        this.requests.forEach(request => request.abort())
+        let f = new FetchRequest("https://maps.googleapis.com/maps/api/place/details/json?&placeid=" + item.place_id + "&key=AIzaSyAJiED9aRjJTSCUHmBE2pUZg4OifcAenpk")
+        this.requests.push(f)
+        f.open().then(response =>
+        {
+            if (response.ok)
+            {
+                response.json().then(json => this.props.onSearch(json))
+                this.props.onChangeText(item.description)
+                this.searchBar.blur()
+            }
+        })
+        .catch(error => console.log(error))
+    }
+
+    _onChangeText = (text) =>
+    {
+        this.requests.forEach(request => request.abort())
+        if (text.length > 2)
+        {
+            let f = new FetchRequest("https://maps.googleapis.com/maps/api/place/autocomplete/json?&input=" + text + "&key=AIzaSyAJiED9aRjJTSCUHmBE2pUZg4OifcAenpk");
+            this.requests.push(f);
+            f.open()
+                .then(response =>
+                {
+                    if (response.ok)
+                    {
+                        response.json().then(json => this.setState({ predictions: json.predictions }))
+                    }
+                })
+                .catch(error => console.log(error))
+        }
+        this.props.onChangeText(text)
+    }
+
+    render()
+    {
+        return (
+            <View>
+                <TextInput ref={component => this.searchBar = component} onBlur={() => { this.setState({ focusOnBar: false }) }} onFocus={() => { this.setState({ focusOnBar: true }) }} value={this.props.value} onChangeText={this._onChangeText} placeholder={"Rechercher un lieu..."} />
+                {
+                    this.state.focusOnBar ?
+                        <FlatList
+                            keyboardShouldPersistTaps={"handled"}
+                            data={this.state.predictions}
+                            keyExtractor={(item, index) => item.id}
+                            renderItem={({ item }) =>
+                            {
+                                return (
+                                    <TouchableOpacity style={{ margin: 5 }} onPress={() => { this._onSearch(item) }}>
+                                        <Text>{item.description}</Text>
+                                    </TouchableOpacity>
+                                )
+                            }
+                            }
+                        />
+                        :
+                        null
+                }
+            </View>
+
+        );
+    }
+}
+
+export default GooglePlacesSearchBar
+
