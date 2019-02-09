5efb480b85df018223e76260df3cc6334c236bb2
Strohl Nicolas <strohl@c-koya.tech>
2019-02-09 13:14:47 +0100

    Changement design de la barre de recherche lieux

--- GeoRally/Components/GooglePlacesSearchBar.js
+++ GeoRally/Components/GooglePlacesSearchBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { TextInput, View, FlatList, Text, TouchableOpacity, Keyboard } from 'react-native';
 import FetchRequest from "../Tools/FetchRequest"
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Colors from '../Colors';
 
 class GooglePlacesSearchBar extends React.Component
 {
@@ -16,7 +18,7 @@ class GooglePlacesSearchBar extends React.Component
 
     componentDidMount()
     {
-        Keyboard.addListener("keyboardDidHide", () => {this.searchBar.blur()})
+        Keyboard.addListener("keyboardDidHide", () => { this.searchBar.blur() })
     }
 
     _onSearch = (item) => 
@@ -33,53 +35,64 @@ class GooglePlacesSearchBar extends React.Component
                 this.searchBar.blur()
             }
         })
-        .catch(error => console.log(error))
+            .catch(error => console.log(error))
     }
 
     _onChangeText = (text) =>
     {
         this.requests.forEach(request => request.abort())
-        if (text.length > 2)
-        {
-            let f = new FetchRequest("https://maps.googleapis.com/maps/api/place/autocomplete/json?&input=" + text + "&key=AIzaSyAJiED9aRjJTSCUHmBE2pUZg4OifcAenpk");
-            this.requests.push(f);
-            f.open()
-                .then(response =>
+        let f = new FetchRequest("https://maps.googleapis.com/maps/api/place/autocomplete/json?&input=" + text + "&key=AIzaSyAJiED9aRjJTSCUHmBE2pUZg4OifcAenpk");
+        this.requests.push(f);
+        f.open()
+            .then(response =>
+            {
+                if (response.ok)
                 {
-                    if (response.ok)
-                    {
-                        response.json().then(json => this.setState({ predictions: json.predictions }))
-                    }
-                })
-                .catch(error => console.log(error))
-        }
+                    response.json().then(json => this.setState({ predictions: json.predictions }))
+                }
+            })
+            .catch(error => console.log(error))
+
         this.props.onChangeText(text)
     }
 
     render()
     {
         return (
-            <View>
-                <TextInput ref={component => this.searchBar = component} onBlur={() => { this.setState({ focusOnBar: false }) }} onFocus={() => { this.setState({ focusOnBar: true }) }} value={this.props.value} onChangeText={this._onChangeText} placeholder={"Rechercher un lieu..."} />
-                {
-                    this.state.focusOnBar ?
-                        <FlatList
-                            keyboardShouldPersistTaps={"handled"}
-                            data={this.state.predictions}
-                            keyExtractor={(item, index) => item.id}
-                            renderItem={({ item }) =>
-                            {
-                                return (
-                                    <TouchableOpacity style={{ margin: 5 }} onPress={() => { this._onSearch(item) }}>
-                                        <Text>{item.description}</Text>
-                                    </TouchableOpacity>
-                                )
-                            }
-                            }
-                        />
-                        :
-                        null
-                }
+            <View style={{ flex: 1, backgroundColor: Colors.primary, margin: 5, borderRadius: 3, elevation: 4 }}>
+                <View style={{ flex: 1, margin: 5, borderRadius: 30, backgroundColor: "white" }}>
+                    <View style={{ flex: 1, flexDirection: "row", alignItems: "center", justifyContent: "flex-start", padding: 3 }}>
+                        <View style={{ flex: 9 }}>
+                            <TextInput style={{ padding: 0 }} ref={component => this.searchBar = component} onBlur={() => { this.setState({ focusOnBar: false }) }} onFocus={() => { this.setState({ focusOnBar: true }) }} value={this.props.value} onChangeText={this._onChangeText} placeholder={"Rechercher un lieu..."} selectTextOnFocus={true} />
+                        </View>
+                        <View style={{ flex: 1, alignItems: "flex-end" }}>
+                            <Icon color={Colors.primaryDark} name="search" size={36} />
+                        </View>
+                    </View>
+                </View>
+                <View style={{ flex: 1, backgroundColor: "white", borderBottomLeftRadius: 3, borderBottomRightRadius: 3 }}>
+                    {
+                        this.state.focusOnBar ?
+                            <FlatList
+                                keyboardShouldPersistTaps={"handled"}
+                                data={this.state.predictions}
+                                keyExtractor={(item, index) => item.id}
+                                renderItem={({ item }) =>
+                                {
+                                    return (
+                                        <TouchableOpacity style={{ padding: 10, borderBottomWidth: 1, borderColor: Colors.primaryLight }} onPress={() => { this._onSearch(item) }}>
+                                            <Text style={{ color: Colors.primaryDark }}>{item.description}</Text>
+                                        </TouchableOpacity>
+                                    )
+                                }
+                                }
+                            />
+                            :
+                            null
+                    }
+                </View>
+
+
             </View>
 
         );
