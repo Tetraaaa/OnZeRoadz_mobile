3ac62672a8e604f4dc644a476e8c5d9b2eb4fd17
Strohl Nicolas <strohl@c-koya.tech>
2019-02-28 14:00:16 +0100

    Ajout d'un composant TextInput custom

---
+++ GeoRally/Components/TextInput.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {TextInput} from "react-native";
+import Colors from "../Colors";
+
+class AppTextInput extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            focus:false
+        }
+    }
+
+    _onFocus = () =>
+    {   
+        this.setState({focus:true}, () => {
+            if(this.props.onFocus) this.props.onFocus()
+        })
+    }
+
+    _onBlur = () =>
+    {
+        this.setState({focus:false}, () => {
+            if(this.props.onBlur) this.props.onBlur()
+        })
+    }
+
+    render()
+    {
+        return (
+            <TextInput onFocus={this._onFocus} onBlur={this._onBlur} style={{borderColor:this.state.focus ? Colors.secondaryDark : Colors.secondaryLight, borderWidth:1, borderRadius:3, margin:5}} {...this.props}/>
+        )
+    }
+}
+
+export default AppTextInput;
