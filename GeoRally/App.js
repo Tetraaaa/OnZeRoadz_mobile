ec815bd7cf09de373ae2cbb76ed7149831325d45
Strohl Nicolas <strohl@c-koya.tech>
2019-02-28 15:08:29 +0100

    Ajout persistance
    Ajout de la persistance dans App.js

--- GeoRally/App.js
+++ GeoRally/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Navigation from './Navigation/Navigation'
 import { Provider } from "react-redux"
+import { persistStore } from 'redux-persist'
 import Store from './Store/configureStore'
+import { PersistGate } from 'redux-persist/es/integration/react'
 
 export default class App extends React.Component
 {
     render()
     {
+        let persistor = persistStore(Store)
         return (
-                <Navigation />
+            <Provider store={Store}>
+                <PersistGate persistor={persistor}>
+                    <Navigation />
+                </PersistGate>
+            </Provider>
+
         )
     }
 }
