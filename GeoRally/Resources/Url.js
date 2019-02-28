80bda5bc611922493afcdd4a0267550a301c5536
Strohl Nicolas <strohl@c-koya.tech>
2019-02-28 15:09:17 +0100

    Ajout des Url google aux ressources

---
+++ GeoRally/Resources/Url.js
@@ -0,0 +1,11 @@
+let test = "https://www.onzeroadz.fr/index.php/"
+
+
+let domain = test;
+
+export default URL = {
+    signup:domain + "signup",
+    login:domain + "login",
+    googlePlaces:"https://maps.googleapis.com/maps/api/place/details/json?&placeid=",
+    googleAutocompletion:"https://maps.googleapis.com/maps/api/place/autocomplete/json?&input="
+}
