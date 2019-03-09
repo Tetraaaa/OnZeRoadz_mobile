b9f59d1e0a3ca1c4ed38df448fea44127912bb11
Strohl Nicolas <strohl@c-koya.tech>
2019-03-09 15:31:42 +0100

    Ajout activity indicator au chargement des circuits de l'utilisateur

--- GeoRally/Views/Circuits.js
+++ GeoRally/Views/Circuits.js
@@ -1,21 +1,25 @@
 import React from "react";
-import { View, Text, FlatList, ScrollView } from "react-native";
+import { View, Text, FlatList, ScrollView, ActivityIndicator } from "react-native";
 import { connect } from 'react-redux'
 import FetchRequest from "../Tools/FetchRequest";
 import Url from "../Resources/Url";
 import CircuitListItem from './../Components/CircuitListItem';
+import Colors from "../Colors";
 
 class Circuits extends React.Component
 {
 
-    constructor(props){
+    constructor(props)
+    {
         super(props);
         this.state = {
-            myCircuits: []
+            myCircuits: [],
+            isLoadingCircuits: true
         }
     }
 
-    componentDidMount(){
+    componentDidMount()
+    {
         let f = new FetchRequest(Url.myCircuits);
         f.open()
             .then(response =>
@@ -25,41 +29,52 @@ class Circuits extends React.Component
                     response.json()
                         .then(json =>
                         {
-                            this.setState({myCircuits: json})
+                            this.setState({ myCircuits: json, isLoadingCircuits:false })
                         });
                 }
                 else
                 {
-                    Alert.alert("Erreur lors de la récupération du circuit", "Impossible de télécharger le circuit sélectionné.")
+                    throw new Error("Erreur lors de la récupération des circuits de l'utilisateur")
                 }
             })
             .catch(error =>
             {
-                Alert.alert("Erreur lors de la récupération du circuit", "Impossible de télécharger le circuit sélectionné.")
+                this.setState({ isLoadingCircuits: false })
             })
 
     }
 
-    _playCircuit = (id) => {
-        this.props.navigation.navigate("PlayScreen", {id:id})
+    _playCircuit = (id) =>
+    {
+        this.props.navigation.navigate("PlayScreen", { id: id })
     }
 
     render()
     {
         return (
-            <ScrollView style={{flex:1}}>
-                <Text style={{ backgroundColor: Colors.primary, margin: 5, fontSize:25, borderRadius: 3, color:'rgba(255,255,255,1)', fontFamily: 'Billabong',textAlign: 'center', textAlignVertical:'center'}}>Mes circuits</Text>
-                <FlatList
-                    style={{margin:5}}
-                    data={this.state.myCircuits}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) => <CircuitListItem data={item} />} />
-                <Text style={{ backgroundColor: Colors.primary, margin: 5, fontSize:25, borderRadius: 3, color:'rgba(255,255,255,1)', fontFamily: 'Billabong',textAlign: 'center', textAlignVertical:'center'}}>Circuits téléchargés</Text>
-                <FlatList
-                    style={{margin:5}}
-                    data={this.props.offlineReducer.circuits}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) => <CircuitListItem data={item} downloaded={true} play={(id) => this._playCircuit(id)} />} />
+            <ScrollView style={{ flex: 1 }}>
+                <View style={{ flex: 1 }}>
+                    <Text style={{ backgroundColor: Colors.primary, margin: 5, fontSize: 25, borderRadius: 3, color: 'rgba(255,255,255,1)', fontFamily: 'Billabong', textAlign: 'center', textAlignVertical: 'center' }}>Mes circuits</Text>
+                    {this.state.isLoadingCircuits ?
+                        <View style={{ alignItems: "center", justifyContent: "center" }}>
+                            <Text>Chargement de vos circuits...</Text>
+                            <ActivityIndicator size="large" color={Colors.primary} />
+                        </View>
+                        :
+                        <FlatList
+                            data={this.state.myCircuits}
+                            keyExtractor={(item) => item.id.toString()}
+                            renderItem={({ item }) => <CircuitListItem data={item} />} />
+                    }
+
+                </View>
+                <View style={{ flex: 1 }}>
+                    <Text style={{ backgroundColor: Colors.primary, margin: 5, fontSize: 25, borderRadius: 3, color: 'rgba(255,255,255,1)', fontFamily: 'Billabong', textAlign: 'center', textAlignVertical: 'center' }}>Circuits téléchargés</Text>
+                    <FlatList
+                        data={this.props.offlineReducer.circuits}
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({ item }) => <CircuitListItem data={item} downloaded={true} play={(id) => this._playCircuit(id)} />} />
+                </View>
             </ScrollView>
         );
     }
