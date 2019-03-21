83007eed3ffb557865bdac9e76ee934f10a990f8
Strohl Nicolas <strohl@c-koya.tech>
2019-03-21 17:24:41 +0100

    Ajout méthode reducer progrès circuit

--- GeoRally/Store/Reducers/offlineReducer.js
+++ GeoRally/Store/Reducers/offlineReducer.js
@@ -11,6 +11,16 @@ function offlineReducer(state = initialState, action) {
                 circuits:[...state.circuits, action.value]
             }
             return nextState;
+        case 'UPDATE_PROGRESS':
+            let dc = state.circuits.slice(0);
+            let circuit = dc.find(circuit => circuit.id === action.value.id);
+            dc = dc.filter(item => item.id !== action.value.id);
+            let newCircuit = Object.assign({}, circuit, {progress:action.value.progress})
+            dc = dc.concat(newCircuit);
+            nextState ={
+                ...state,
+                circuits:dc
+            }
         default:
             return state;
     }
