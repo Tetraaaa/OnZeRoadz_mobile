f18fea5caac7537a1d45e675172ba4c88008cea5
Strohl Nicolas <strohl@c-koya.tech>
2019-03-22 15:26:19 +0100

    Ajout props disabled et disabledColor au Button

--- GeoRally/Components/Button.js
+++ GeoRally/Components/Button.js
@@ -12,7 +12,7 @@ class Button extends React.Component
     render()
     {
         return (
-            <TouchableOpacity activeOpacity={0.5} style={[{backgroundColor:this.props.color, elevation:4, borderRadius:2, padding:7}, this.props.style]} onPress={this.onPress} >
+            <TouchableOpacity activeOpacity={0.5} style={[{elevation:4, borderRadius:2, padding:7}, this.props.style, {backgroundColor:this.props.disabled ? this.props.disabledColor : this.props.color}]} onPress={this.onPress} disabled={this.props.disabled} >
                 <Text style={{textAlign:"center", color:"white", fontWeight:"bold"}}>{this.props.title.toUpperCase()}</Text>
             </TouchableOpacity>
         )
@@ -28,7 +28,9 @@ Button.propTypes = {
 Button.defaultProps = {
     onPress:() => {},
     title:"",
-    color:"#2196F3"
+    color:"#2196F3",
+    disabledColor:"lightgray",
+    disabled:false
 }
 
 export default Button;
