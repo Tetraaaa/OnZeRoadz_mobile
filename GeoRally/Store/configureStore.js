aaeb96523634fe754ec51740195bcc2259dba7bb
Strohl Nicolas <strohl@c-koya.tech>
2019-04-25 13:55:56 +0200

    Ajout persistance via fichiers
    oskur

--- GeoRally/Store/configureStore.js
+++ GeoRally/Store/configureStore.js
@@ -4,10 +4,11 @@ import circuitsReducer from "./Reducers/circuitsReducer";
 import offlineReducer from "./Reducers/offlineReducer";
 import {persistCombineReducers} from "redux-persist";
 import storage from 'redux-persist/lib/storage'
+import FilesystemStorage from 'redux-persist-filesystem-storage'
 
 const rootPersistConfig = {
     key: 'root',
-    storage: storage
+    storage: FilesystemStorage
   }
   
   export default createStore(persistCombineReducers(rootPersistConfig, {connectionReducer, circuitsReducer, offlineReducer}))
--- GeoRally/android/app/src/main/java/com/georally/MainApplication.java
+++ GeoRally/android/app/src/main/java/com/georally/MainApplication.java
@@ -3,6 +3,7 @@ package com.georally;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -10,6 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.modules.storage.ReactDatabaseSupplier;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new VectorIconsPackage(),
             new MapsPackage(),
             new RNGestureHandlerPackage()
@@ -47,5 +50,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+
   }
 }
